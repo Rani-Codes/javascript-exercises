e0d4bc1646928cbc2a9ed2b4ef4658e260f3bc0f
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2023-09-04 23:22:07 -0700

    complete exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let multiString = "";
+    for(let i = 0; i < num; i++) {
+        multiString += string;
 
+    }
+    if(num < 0) multiString = "ERROR";
+    
+    return multiString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const myArray = input.split("");
+    let endL = myArray.length - 1;
+    let reverseString = '';
+    for(let i = 0; i < myArray.length; i++) {
+        reverseString += myArray[endL];
+        endL--;
+    }
+    return reverseString;
 };
 
 // Do not edit below this line

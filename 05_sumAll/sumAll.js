c2beddab2e1a2aefc79fe6e02ad4585cffa60dae
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2023-09-06 13:01:59 -0700

    exercises 5 and 6 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
 
+    if(typeof(startNum) !== "number" || typeof(endNum) !== "number") {
+        return "ERROR";
+    }
+    
+    if(startNum < 0 || endNum < 0) {
+        return "ERROR";
+    }
+
+    let max = 0;
+    if(startNum > endNum) {
+        max = startNum;
+        startNum = endNum;
+        endNum = max;
+    }
+
+    for(let i = startNum; i <= endNum; endNum--) {
+        sum += endNum;
+    }
+    console.log(typeof(startNum));
+    return sum;
 };
 
 // Do not edit below this line

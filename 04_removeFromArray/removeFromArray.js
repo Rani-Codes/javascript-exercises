5ab01531ab677d7c50d92dcf7976e44ff6fa569b
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2023-09-06 10:35:14 -0700

    exercise 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const inputArray = [];
 
-};
+const removeFromArray = function(inputArray) {
+    let rmvArray = Array.from(arguments)
+
+    for(j = 1; j < rmvArray.length; j++) {
+        for(i = 0; i < inputArray.length; i++) {
+            if(inputArray[i] === rmvArray[j]) {
+                inputArray.splice(i, 1);
+            }
+        }
+    }
+    return inputArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
